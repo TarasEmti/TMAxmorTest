a2abc487ac56a38c501ff921ec172ed0dd42611b
Тарас Минин <elouseman@gmail.com>
2016-07-17 00:05:06 +0700

    Initial Commit

---
+++ TMAxmorTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TMAxmorTest
+//
+//  Created by Тарас on 17.07.16.
+//  Copyright © 2016 Тарас. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TMAxmorTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TMAxmorTest
+//
+//  Created by Тарас on 17.07.16.
+//  Copyright © 2016 Тарас. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
